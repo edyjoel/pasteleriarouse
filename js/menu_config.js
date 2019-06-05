d66b502775ddde79a3acf7ded173d80b261bfc46
Edy Joel <edyxicon@gmail.com>
2019-06-05 00:07:31 -0600

    Agregando el menu y funcionalidades del menu

---
+++ js/menu_config.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  
+  $('#toggleMenu').on('click', function(e){
+    $( ".items_nav_movil" ).toggle( "slow" );
+  });
+
+  $('.item_nav').on('click', function(e) {
+  
+    var element = e.currentTarget.hash;
+    $(element).addClass('padding_top');
+  })
+
+
+})
+
+
+$(document).ready(function() {
+  $('a[href^="#"]').click(function() {
+    var destino = $(this.hash);
+    if (destino.length == 0) {
+      destino = $('a[name="' + this.hash.substr(1) + '"]');
+    }
+    if (destino.length == 0) {
+      destino = $('html');
+    }
+    $('html, body').animate({ scrollTop: destino.offset().top }, 500);
+    return false;
+  });
+});
