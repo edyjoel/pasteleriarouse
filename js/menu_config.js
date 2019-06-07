5222656764c082364091b44536e30d00de2cda13
Edy Joel <edyxicon@gmail.com>
2019-06-07 00:21:20 -0600

    Agregando galeria

--- js/menu_config.js
+++ js/menu_config.js
@@ -18,51 +18,52 @@ $(document).ready(function(){
 })
 
 var hero = document.querySelector('.hero').offsetTop ;
-var productos = document.querySelector('#productos').offsetTop -80;
-var cocinamos = document.querySelector('#cocinamos_amor').offsetTop-80;
-var nosotros = document.querySelector('#sobre_nosotros').offsetTop-80;
-var testimonios = document.querySelector('#testimonios').offsetTop-80;
-var ordenar = document.querySelector('#ordenar').offsetTop-80;
+var productos = document.querySelector('#productos').offsetTop -100;
+var cocinamos = document.querySelector('#cocinamos_amor').offsetTop-100;
+var nosotros = document.querySelector('#sobre_nosotros').offsetTop-100;
+var testimonios = document.querySelector('#testimonios').offsetTop-100;
+var galeria = document.querySelector('#galeria').offsetTop-100;
+var ordenar = document.querySelector('#ordenar').offsetTop-100;
 
 window.addEventListener('scroll', function(){
 
   if( hero < window.scrollY ) {
-    console.log('arriba')
     document.querySelector('.logo img').style.maxWidth="70px";
   }else {
     document.querySelector('.logo img').style.maxWidth="100px";
   }
 
   if( productos < window.scrollY && (productos + document.querySelector('.hero').offsetHeight) > window.scrollY ) {
-    console.log('arriba')
     document.querySelector('#nav_productos').style.borderBottom="5px solid #FC6170";
   }else {
     document.querySelector('#nav_productos').style.borderBottom="5px solid transparent";
   }
 
   if( cocinamos < window.scrollY && (cocinamos + document.querySelector('.cocinamos_amor').offsetHeight) > window.scrollY ) {
-    console.log('arriba')
     document.querySelector('#nav_cocinamos').style.borderBottom="5px solid #FC6170";
   }else {
     document.querySelector('#nav_cocinamos').style.borderBottom="5px solid transparent";
   }
 
   if( nosotros < window.scrollY && (nosotros + document.querySelector('.sobre_nosotros').offsetHeight) > window.scrollY ) {
-    console.log('arriba')
     document.querySelector('#nav_nosotros').style.borderBottom="5px solid #FC6170";
   }else {
     document.querySelector('#nav_nosotros').style.borderBottom="5px solid transparent";
   }
 
   if( testimonios < window.scrollY && (testimonios + document.querySelector('.testimonios').offsetHeight) > window.scrollY ) {
-    console.log('arriba')
     document.querySelector('#nav_testimonios').style.borderBottom="5px solid #FC6170";
   }else {
     document.querySelector('#nav_testimonios').style.borderBottom="5px solid transparent";
   }
 
+  if( galeria < window.scrollY && (galeria + document.querySelector('.galeria').offsetHeight) > window.scrollY ) {
+    document.querySelector('#nav_galeria').style.borderBottom="5px solid #FC6170";
+  }else {
+    document.querySelector('#nav_galeria').style.borderBottom="5px solid transparent";
+  }
+
   if( ordenar < window.scrollY && (ordenar + document.querySelector('.ordenar').offsetHeight) > window.scrollY ) {
-    console.log('arriba')
     document.querySelector('#nav_contacto').style.borderBottom="5px solid #FC6170";
   }else {
     document.querySelector('#nav_contacto').style.borderBottom="5px solid transparent";
